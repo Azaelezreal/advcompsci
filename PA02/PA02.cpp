e213c407d1705c6dfb8771e872941506c8f4515b
Azael Mayer <amayer@micds.org>
2023-08-24 09:49:03 -0500

    finished error messages PA02

--- PA02/PA02.cpp
+++ PA02/PA02.cpp
@@ -7,7 +7,17 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
     vector<int> count(10, 0);
-    std::ifstream inputFile("PA02SampleFile.txt");
+    string file;
+    if (argc > 1) {
+        // Print the first argument
+        file = argv[1];
+    } else {
+        cerr<< "No arguments provided." <<endl;
+        return 2;
+    }
+
+    std::ifstream inputFile(file);
+
     if (!inputFile.is_open()) {
         std::cerr << "Failed to open the file." << std::endl;
         return 1; // or some other error code
