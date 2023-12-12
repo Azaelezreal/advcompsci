3b75fa2e2a54e346356f78986de0c5725cf9db8a
Azael Mayer <amayer@micds.org>
2023-12-12 09:31:42 -0600

    pa12 helper functions almost done

--- PA12/BinarySearch.cpp
+++ PA12/BinarySearch.cpp
@@ -1,12 +1,101 @@
 #include <iostream>
 #include "BinarySearch.h"
+#include <fstream>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
-BinarySearch() : head(nullptr) {}
+BinarySearch::BinarySearch() : head(nullptr) {}
 
+void BinarySearch::insert (std::string data, Node* currNode) {
+	if (currNode == nullptr) {
+		currNode = new Node(data);
+	} else if (data == currNode->word) {
+		currNode->times++;
+    } else if (data<currNode->word) {
+		insert(data, currNode->left);
+	} else if (data>currNode->word) {
+		insert(data, currNode->right);
+	}
+	
+};
 
+void BinarySearch::remove (Node* currNode) {
+	if (currNode == nullptr) {
+		delete currNode;
+	}
+	else if (currNode->left != nullptr) {
+		remove(currNode->left);
+	}
+	else if (currNode->right != nullptr) {
+		remove(currNode->right);
+	}
+};
 
-int main() {
+void BinarySearch::display(Node* currNode) {
+    if (currNode != nullptr) {
+
+    }
+    cout << head->word << " ";
+}
+
+std::string toLowerCase(const std::string& input) {
+    std::string result = input;
+    for (char& c : result) {
+        c = std::tolower(c);
+    }
+    return result;
+}
+
+string removeCharacters(const std::string& input) {
+    std::string result;
+    for (char c : input) {
+        // Check if the character is not in the set of specified characters
+        if (c != '.' && c != ',' && c != ';' && c != '!' && c != '?' && c != '(' && c != ')' && c != '[' && c != ']') {
+            result += c;
+        }
+    }
+    return result;
+}
+
+int BinarySearch::getDepth(std::string data, Node* currNode, int depth) {
+    if (currNode == nullptr) {
+        return -1;
+    } else if (currNode->word == data) {
+        currNode->times++;
+    }
+    else if (currNode->word > data) {
+        return getDepth(data, currNode->left, depth+1);
+    }
+    else {
+        return getDepth(data, currNode->right, depth+1);
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    string file;
+    string input;
+    string target;
+    if (argc >2 ) {
+        input = argv[1];
+        target = argv[2];
+    } else {
+        cerr<< "Arguments not provided." <<endl;
+        exit(1);
+    }
+    ifstream inputFile(input);
+
+    if (!inputFile.good()) {
+        cerr << "Failed to open input file." << endl;
+        exit(1);
+    }
+
+    
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        line = removeCharacters(line);
+    }
+    inputFile.close();
 
 }
--- PA12/BinarySearch.h
+++ PA12/BinarySearch.h
@@ -1,19 +1,35 @@
 #ifndef BINARYSEARCH_H
 #define BINARYSEARCH_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+
 
 using namespace std;
+
+struct Node {
+	std::string word;
+	int times;
+	Node* left;
+	Node* right;
+
+    Node(std::string data) : word(data), left(nullptr), right(nullptr) {}
+};
 
 struct BinarySearch {
     BinarySearch();
     ~BinarySearch();
 
-	string letter;
+	std::string letter;
 	int times;
 	BinarySearch* left;
 	BinarySearch* right;
 
-    void insert(int data);
-    void display() const;
+    void insert(std::string data, Node* currNode);
+    void remove(Node* currNode);
+    void display(Node* currNode);
+    int getDepth(std::string data, Node* currNode, int depth);
 
     BinarySearch* head;
 };
